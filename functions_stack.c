4c415bb9faa8c0e48bcee540877f0648035cc2ad
Rcastro89 <r_castro_m@yahoo.es>
2022-01-13 00:50:16 -0500

    pop

--- functions_stack.c
+++ functions_stack.c
@@ -108,7 +108,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (!*stack)
 	{
-		fprintf(stderr, "L%d: can't pop an empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		free(sin_espacios);
 		free_dlist(head);
 		free(lines);
